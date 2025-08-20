4db1653ae2e9d2af6224c45bb46e91c72b0d5b4b
Ankit kumar <ankitgupta2ndsp@gmail.com>
2025-08-20 06:54:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from routes import admin
 
-from routes import user, admin
+app = FastAPI(title="Komal API", description="Super Fast API 🚀", version="1.0.0")
 
-app = FastAPI(title="Komal API", description="Super Fast Music API 🚀", version="1.0.0")
-
-# templates
 templates = Jinja2Templates(directory="templates")
 
 # include routers
-app.include_router(user.router, prefix="/api", tags=["User API"])
 app.include_router(admin.router, prefix="/admin", tags=["Admin Panel"])
 
 @app.get("/", response_class=HTMLResponse)
